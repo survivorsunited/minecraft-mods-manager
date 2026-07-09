c5c1d586df1de8f45d10bf967a616f539344575a
Max Barrass <max.barrass@gmail.com>
2026-07-09 20:55:23 +1000

    Pin Basic Storage 2.0.30 for 1.21.11

--- src/Patches/Pin-12111ModVersions.ps1
+++ src/Patches/Pin-12111ModVersions.ps1
@@ -4,13 +4,13 @@ if (-not $script:OriginalGetModListBefore12111Pins -and (Get-Command Get-ModList
 
 function Set-12111BasicStoragePin {
     param($Row)
-    $url = 'https://github.com/survivorsunited/mod-basic-storage/releases/download/2.0.29/basic-storage-2.0.29+1.21.11.jar'
+    $url = 'https://github.com/survivorsunited/mod-basic-storage/releases/download/2.0.30/basic-storage-2.0.30+1.21.11.jar'
     $Row.CurrentGameVersion = '1.21.11'
-    $Row.CurrentVersion = '2.0.29'
+    $Row.CurrentVersion = '2.0.30'
     $Row.CurrentVersionUrl = $url
-    $Row.Jar = 'basic-storage-2.0.29+1.21.11.jar'
+    $Row.Jar = 'basic-storage-2.0.30+1.21.11.jar'
     $Row.LatestGameVersion = '1.21.11'
-    $Row.LatestVersion = '2.0.29'
+    $Row.LatestVersion = '2.0.30'
     $Row.LatestVersionUrl = $url
     $Row.Url = 'https://github.com/survivorsunited/mod-basic-storage'
     $Row.Host = 'github'
